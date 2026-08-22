c1cb00ebe0cd8ffd36b694d268dd55c34e87cefd
vippartypods-ops <vippartypods@gmail.com>
2026-08-22 11:59:03 -0400

    Add Zodiac Box configuration

---
+++ assets/config.js
@@ -0,0 +1,17 @@
+window.ZODIAC_CONFIG = {
+  supabaseUrl: 'https://wiyzwivpngxnqmjyubgr.supabase.co',
+  supabasePublishableKey: 'sb_publishable_TzB7iNseAVUv3JoQrlfiBA_Z_Sww5eB',
+  brandName: 'The Zodiac Box',
+  claimNote: 'GAME PRIZE WON',
+  timezone: 'America/New_York',
+  tiktokLoginEnabled: false,
+  demoPreviewEnabled: true,
+  paths: {
+    home: '/',
+    monday: '/monday/',
+    wednesday: '/wednesday/',
+    friday: '/friday/',
+    weekend: '/weekend/',
+    admin: '/admin/'
+  }
+};
