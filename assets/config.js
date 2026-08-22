46680098fe65eae3461b3aea0abc4e20e82492f7
vippartypods-ops <vippartypods@gmail.com>
2026-08-22 12:29:33 -0400

    Add TikTok Shop destination

--- assets/config.js
+++ assets/config.js
@@ -3,6 +3,7 @@ window.ZODIAC_CONFIG = {
   supabasePublishableKey: 'sb_publishable_TzB7iNseAVUv3JoQrlfiBA_Z_Sww5eB',
   brandName: 'The Zodiac Box',
   claimNote: 'GAME PRIZE WON',
+  shopUrl: 'https://vt.tiktok.com/ZTDuB4sXB/?page=TikTokShop&utm_campaign=client_share&utm_source=messenger&share_app_id=1233',
   timezone: 'America/New_York',
   tiktokLoginEnabled: false,
   demoPreviewEnabled: true,
